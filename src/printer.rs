5cb7e68878e2b9765f46ddc2cf2f0ab708279939
David Timms <davidtimms@btinternet.com>
2024-07-10 00:19:44 +0100

    Refactored printer ready for indentation

--- src/printer.rs
+++ src/printer.rs
@@ -1,21 +1,36 @@
 use std::{
     collections::BTreeMap,
-    fmt::{self, Display, Formatter, Write},
+    fmt::{self, Write},
 };
 
 use crate::ast::Json;
 
 impl fmt::Display for Json {
     fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        match self {
-            Json::Null => formatter.write_str("null"),
-            Json::Boolean(true) => formatter.write_str("true"),
-            Json::Boolean(false) => formatter.write_str("false"),
-            Json::String(string) => formatter.write_str(&display_json_string(string)),
-            Json::Number(number) => formatter.write_fmt(format_args!("{number}")),
-            Json::Array(array) => display_json_array(array, formatter),
-            Json::Object(object) => display_json_object(object, formatter),
-        }
+        display_json(self, formatter, 2, 0)
+    }
+}
+
+pub fn json_to_string(value: &Json, indent: u64) -> String {
+    let mut output = String::new();
+    display_json(value, &mut output, indent, 0).expect("Failed to write JSON to string");
+    output
+}
+
+fn display_json<W: Write>(
+    value: &Json,
+    output: &mut W,
+    indent: u64,
+    level: u64,
+) -> Result<(), fmt::Error> {
+    match value {
+        Json::Null => output.write_str("null"),
+        Json::Boolean(true) => output.write_str("true"),
+        Json::Boolean(false) => output.write_str("false"),
+        Json::String(string) => output.write_str(&display_json_string(string)),
+        Json::Number(number) => output.write_fmt(format_args!("{number}")),
+        Json::Array(array) => display_json_array(array, output, indent, level),
+        Json::Object(object) => display_json_object(object, output, indent, level),
     }
 }
 
@@ -42,35 +57,52 @@ fn display_json_string(string: &str) -> String {
     escaped
 }
 
-fn display_json_array(items: &Vec<Json>, formatter: &mut Formatter) -> Result<(), fmt::Error> {
-    formatter.write_char('[')?;
+fn display_json_array<W: Write>(
+    items: &Vec<Json>,
+    output: &mut W,
+    indent: u64,
+    level: u64,
+) -> Result<(), fmt::Error> {
+    output.write_char('[')?;
 
     for (index, item) in items.into_iter().enumerate() {
         if index > 0 {
-            formatter.write_char(',')?;
+            output.write_char(',')?;
         }
-        item.fmt(formatter)?;
+        display_json(item, output, indent, level)?;
     }
 
-    formatter.write_char(']')?;
+    output.write_char(']')?;
     Ok(())
 }
 
-fn display_json_object(
+fn display_json_object<W: Write>(
     object: &BTreeMap<String, Json>,
-    formatter: &mut Formatter,
+    output: &mut W,
+    indent: u64,
+    level: u64,
 ) -> Result<(), fmt::Error> {
-    formatter.write_char('{')?;
+    output.write_char('{')?;
 
     for (index, (key, value)) in object.into_iter().enumerate() {
         if index > 0 {
-            formatter.write_char(',')?;
+            output.write_char(',')?;
         }
-        formatter.write_str(&display_json_string(&key))?;
-        formatter.write_char(':')?;
-        value.fmt(formatter)?;
+        output.write_str(&display_json_string(&key))?;
+        output.write_char(':')?;
+        display_json(value, output, indent, level)?;
     }
 
-    formatter.write_char('}')?;
+    output.write_char('}')?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{ast::Json, printer::json_to_string};
+
+    #[test]
+    fn it_prints_null() {
+        assert_eq!(json_to_string(&Json::Null, 2), "null");
+    }
 }
